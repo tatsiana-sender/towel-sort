82a547ab086d112f74416be3664a4c2a8fe05ebd
Tatsiana <49600781+tatsiana-sender@users.noreply.github.com>
2020-03-22 21:25:18 +0300

    add decise of task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix == undefined) {
+        return [];
+    } else {
+        var array = [];
+        for (i = 0; i < matrix.length; i++) {
+            if (i == 0 || i % 2 == 0) {
+                array.push(matrix[i]) ;
+          } else {
+              array.push(matrix[i].reverse()) ;
+           }
+       };
+       return array.flat();
+    };
 }
